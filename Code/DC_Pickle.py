e0aafdca83562bba8e95bbce3b316a8ff90fe8d5
Gang SeongMuk <PureMac@MacBook-Air.local>
2016-08-09 01:03:48 +0100

    9Aug01h03m

---
+++ Code/DC_Pickle.py
@@ -0,0 +1,61 @@
+import pandas as pd #data analysis
+import os #file/directory operations
+# import multiprocessing as mp
+from six.moves import cPickle as pickle
+
+def make_Pickle(set_data, set_filename, force = False):
+    if os.path.exists(set_filename) and not force:
+        # You may override by setting force=True.  
+        print('%s already present - Skipping pickling.' % set_filename)
+    else :
+        try :
+            with open(set_filename, 'wb') as f:
+                pickle.dump(set_data, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e :
+            print("Unable to save data to", set_filename, ': ', e)
+            return
+
+def make_folders(dir_name):
+    set_dir_name = dir_name
+
+    if os.path.exists(set_dir_name):
+      # You may override by setting force=True.
+      print('%s already present - Skipping pickling.' % set_dir_name)
+    else:
+        try:
+            os.makedirs(set_dir_name)
+        except Exception as e:
+            print('Unable to make', set_dir_name, ':', e)
+            return
+
+def open_Pickle(pickle_path):
+    try :
+        with open(pickle_path, 'rb') as f:
+            train = pickle.load(f)
+            return train
+    except Exception as e :
+        print("Unable to save data to", pickle_path, ': ', e)
+        return
+            
+def seperate_indi(groups, num_cores, dir_name):
+    nFiles = round(len(groups)/num_cores)
+    
+    print(nFiles)
+    
+    # set_dir_name = "pickles/individuals/process{0}".format(i)
+    
+    groups_dict = dict(list(groups))
+    
+    count = 0
+    for i in range(num_cores):
+        set_file_name = dir_name.format(i)
+        
+        if count < num_cores-1:
+            gb = pd.concat(list(groups_dict.values())[nFiles*i:nFiles*(i+1)])
+            make_Pickle(gb, set_file_name)
+            print('inside')
+        else:
+            print('last!')
+            gb = pd.concat(list(groups_dict.values())[nFiles*i:])
+            make_Pickle(gb, set_file_name)
+        count = count + 1
---
+++ Code/DC_filter.py
@@ -0,0 +1,120 @@
+import numpy as np #numerical function
+import datetime
+import multiprocessing as mp
+
+cores= mp.cpu_count() #processes raw data in batches (required because data>ram)
+loding = True
+
+## parameters
+long_gap_length=7*60
+gap_window=5*60 #ie long gaps are 7 hours<gap<11 hours
+shor_gap_length=15 #analysis extremely robust to changes in this parameter
+
+#guesses at what bounds waking time across the population
+early_rising=5
+late_rising=12
+
+#guesses at what bounds bedtime across the population
+early_daytim=early_rising+12
+late_daytim=late_rising+12
+
+
+def add_TotalPlays(data_frame):
+    # add players total number of attempts to df
+    data_frame['total_plays'] = len(data_frame)
+    return data_frame
+
+def add_GapType(df):
+    df['localtime']=np.around((df['ga:hour']+df['ga:longitude']*24/360)%24 , decimals=1)
+    df['long_gap']=(df['diff_time']>long_gap_length) & (df['diff_time']<(long_gap_length+gap_window))
+    df['shor_gap']=(df['diff_time']<shor_gap_length)
+    df['sleepgap']=df['long_gap'] & ((df['localtime']>early_rising) & (df['localtime']<late_rising))
+    return df
+
+def add_GapCategory(df):
+    '''categorise people by their gapness'''
+    '''here define range over which we define taking gaps'''
+    #dfp=df[6:8] #tight range, on game 7
+    dfp=df[1:15] #any of first 15 games
+    
+    if len(dfp.shor_gap)==sum(dfp.shor_gap):
+        gaptype=1
+    elif (sum(dfp.long_gap)==1) & (sum(dfp.sleepgap)==0):
+        gaptype=2
+    elif (sum(dfp.long_gap)==1) & (sum(dfp.sleepgap)==1):
+        gaptype=3
+    else:
+        gaptype=4
+    
+    df['gaptype']=gaptype
+
+    return df
+
+def add_DiffTime(df):
+    for index, row in df.iterrows():
+        '''
+        if not index%10:
+            print("{0}: {1}%".format(str(queue), round(index/len(new_df)*100)))
+        
+        count = count+1
+        '''
+        ## combine day, hour and minute into datetime object (ie absolute time)
+        str_date_time = "{0}:{1}:{2}".format(str(int(row['ga:date'])), str(int(row['ga:hour'])), str(int(row['ga:minute'])))
+        str_to_time = datetime.datetime.strptime(str_date_time, "%Y%m%d:%H:%M")
+        
+        df.loc[index, 'comb_time'] = str_to_time
+        
+        if df.loc[index,'ga:eventLabel']==1:
+            df.loc[index, 'diff_time'] = np.NaN
+        else:
+            try:
+                diff_time = (df.loc[index,'comb_time'].to_datetime() - df.loc[index-1,'comb_time'].to_datetime()).total_seconds()/60
+                df.loc[index, 'diff_time'] = diff_time
+            except:
+                df.loc[index, 'diff_time'] = np.NaN
+    return df
+
+def check_Discon(df, name_vec):
+    count = 0
+    groups = df.groupby('ga:eventAction')
+    
+    for name, group in groups:
+        df_len = len(group)
+        eventLabel = np.ones(df_len)
+        eventLabel[:] = group['ga:eventLabel']
+        diff_AlOm = eventLabel[df_len-1]-eventLabel[0]+1
+        
+        if not diff_AlOm == df_len:
+            name_vec.append(name)
+        count = count+1
+
+def filter_time(df):
+    df=df[~(df['ga:latitude']==0)]
+    '''remove data with negative time differences'''
+    '''cannot work out why some time differences are <0, but have verified that >0 values are correct'''
+    df=df[~(df.diff_time<0)]
+    
+    return df
+
+def rm_GaInCols(data_frame): # remove ga:
+    cols = np.array(data_frame.columns)
+    count = 0
+    for elmt in cols:
+        if 'ga:' in elmt:
+            cols[count] = elmt.strip('ga:')
+        count = count + 1
+    return cols
+
+def data_PostProcess(df, err_plys):
+    df.sort_values(by=['ga:eventAction', 'ga:eventLabel'], inplace=True) # mac
+    #df.sort(columns=['ga:eventAction', 'ga:eventLabel'],inplace=True) # school computer
+    df = df.reset_index()
+    err_plys = err_plys.reset_index()
+    
+    df = df.drop('index', axis=1)
+    df.columns = rm_GaInCols(df) # remove ga:
+    
+    err_plys = err_plys.drop('index', axis=1)
+    err_plys.columns = ['player ID']
+    
+    return df, err_plys
