c06c613a188abc33a8f15b741c6298f2b55dcb1f
Gang SeongMuk <liprin1129@gmail.com>
2016-08-10 18:40:34 +0100

    modification: curve fitting ipython

--- Code/Curve_Functions.py
+++ Code/Curve_Functions.py
@@ -3,8 +3,17 @@ import numpy as np
 def polynomial_curve(x, w0, w1):
     return w0 + w1*x
 
-def powerlaw_curve(x, w0, w1, w2):
-    return w0 * np.power(x, -w1) + w2 # -w0 * x**(w1) + w2
+def powerlaw_curve2(x, w0, w1):
+    return -w0 * (x)**(-w1) # -w0 * np.power((x), w1) + w2
 
-def exponential_curve(x, a, b, c):
+def powerlaw_curve3(x, w0, w1, w2):
+    return -w0 * (x)**(-w1) + w2 # -w0 * np.power((x), w1) + w2
+
+def powerlaw_curve4(x, w0, w1, w2, w3):
+    return -w0 * (x+w3)**(-w1) + w2 # -w0 * np.power((x), w1) + w2
+
+def exponential_curve2(x, a, b):
+    return a * np.exp(-b * x)
+
+def exponential_curve3(x, a, b, c):
     return a * np.exp(-b * x) + c
