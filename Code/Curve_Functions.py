2e540cbde49e82cabaff1cec461e7ccac5052dc2
PureVAIO <liprin1129@gmail.com>
2016-08-11 00:12:47 +0100

    VAIO_funtions

--- Code/Curve_Functions.py
+++ Code/Curve_Functions.py
@@ -1,7 +1,36 @@
 import numpy as np
 
+############################################
+## Polynomial Functioin
+############################################
+
 def polynomial_curve(x, w0, w1):
     return w0 + w1*x
+
+def polynomial_least(w, x, y):
+    return polynomial_curve(x, w[0], w[1]) - y
+
+
+############################################
+## Exponential Functioin
+############################################
+
+def exponential_curve2(x, a, b):
+    return a * np.exp(-b * x)
+
+def exponential_curve3(x, a, b, c):
+    return a * np.exp(-b * x) + c
+
+def exponential_least2(w, x, y):
+    return exponential_curve2(x, w[0], w[1]) - y
+
+def exponential_least3(w, x, y):
+    return exponential_curve3(x, w[0], w[1], w[2]) - y
+
+
+############################################
+## Powerlaw Functioin
+############################################
 
 def powerlaw_curve2(x, w0, w1):
     return -w0 * (x)**(-w1) # -w0 * np.power((x), w1) + w2
@@ -11,9 +40,12 @@ def powerlaw_curve3(x, w0, w1, w2):
 
 def powerlaw_curve4(x, w0, w1, w2, w3):
     return -w0 * (x+w3)**(-w1) + w2 # -w0 * np.power((x), w1) + w2
-
-def exponential_curve2(x, a, b):
-    return a * np.exp(-b * x)
-
-def exponential_curve3(x, a, b, c):
-    return a * np.exp(-b * x) + c
+    
+def powerlaw_least2(w, x, y):
+    return powerlaw_curve2(x, w[0], w[1]) - y
+    
+def powerlaw_least3(w, x, y):
+    return powerlaw_curve3(x, w[0], w[1], w[2]) - y
+    
+def powerlaw_least4(w, x, y):
+    return powerlaw_curve4(x, w[0], w[1], w[2], w[3]) - y
