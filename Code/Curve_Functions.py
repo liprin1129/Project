4f56bec904a266a1231e70d36126745d861b62d0
Gang SeongMuk <liprin1129@gmail.com>
2016-08-14 23:24:25 +0100

    updating...: curve fitting

--- Code/Curve_Functions.py
+++ Code/Curve_Functions.py
@@ -55,16 +55,16 @@ def powerlaw_least4(w, x, y):
     return powerlaw_curve4(x, w[0], w[1], w[2], w[3]) - y
     
 ############################################
-## About curve fitting
+## About single curve fitting
 ############################################
     
 def cost_Function(true_y, pred_y):
     diff = np.power((true_y - pred_y), 2)
-    cost = np.sum(diff)
+    cost = np.sum(diff)/2
     return cost
 
 def curve_Fitting(least_func, curve_func, x, y, seed, file_path, clt_num):
-    fig, ax = plt.subplots(1, 1, figsize=(10,8))
+    fig, ax = plt.subplots(1, 1, figsize=(6,4))
     
     # popt, pcov = curve_fit(func, x, y, maxfev = 1000000)
     '''
@@ -75,7 +75,7 @@ def curve_Fitting(least_func, curve_func, x, y, seed, file_path, clt_num):
         lower_bound.append(popt[i] - pcov[i,i])
     '''
     
-    x_fit = np.linspace(0, 15, 100)
+    x_fit = np.linspace(0, 16, 100)
     '''
     if seed == 1:
         lsq = least_squares(least_func, seed, args=(x, y))
@@ -118,3 +118,78 @@ def curve_Fitting(least_func, curve_func, x, y, seed, file_path, clt_num):
     fig.savefig(file_path, dpi=100)
     
     return lsq.x, cost
+    
+    
+############################################
+## About multipe curve fitting
+############################################
+    
+def multi_curveFitting_2(least_func, clt, avg, seed, n_param=2):
+    cost = []
+    param1 = np.ones((n_param, 300))
+    param2 = np.ones((n_param, 300))
+    
+    for n in range(300): # iteration for all data
+        # print("iteration ", n)
+
+        x1 = np.linspace(1, n, n)
+        x2 = np.linspace(n+1, 300, 300-n)
+
+        y1 = avg[:n]
+        y2 = avg[n:]
+
+        lsq1 = least_squares(least_func, seed, args=(x1, y1))
+        lsq2 = least_squares(least_func, seed, args=(x2, y2))
+
+        cost1 = lsq1.cost
+        cost2 = lsq2.cost
+        
+        param1[:, n] = lsq1.x
+        param2[:, n] = lsq2.x
+
+        cost.append(cost1+cost2)
+        
+    idx = np.argmin(cost)
+    return idx, cost[idx], param1[:, idx], param2[:, idx]
+    
+def multi_curveFitting_3(least_func, avg, seed, n_param=2):
+    cost = []
+    param1 = np.ones((n_param, 300))
+    param2 = np.ones((n_param, 300, 300))
+    param3 = np.ones((n_param, 300, 300))
+    
+    for n1 in range(300): # iteration for all data
+        n1 = n1+1
+        print("iter ", n1)
+        x1 = np.linspace(1, n1, n1)
+        y1 = avg[:n1]
+
+        lsq1 = least_squares(least_func, seed, args=(x1, y1))
+        cost1 = lsq1.cost
+        param1[:, n1] = lsq1.x 
+        
+        cost_remain = []        
+        for n2 in range(300-n1):
+            n2 = n2+1
+            x2 = np.linspace(n1+1, n1+n2, n2)
+            x3 = np.linspace(n2+2, 300, 300-n1-n2)
+            print("x1:{0}, x2:{1}, x3:{2}".format(x1, x2, x3))
+            
+            '''
+            y2 = avg[n1:n1+n2]
+            y3 = avg[n1+n2:]
+            
+            lsq2 = least_squares(least_func, seed, args=(x2, y2))
+            lsq3 = least_squares(least_func, seed, args=(x3, y3))
+    
+            cost_remain.append(lsq2.cost+lsq3.cost)
+
+            param2[:, n1, n2] = lsq2.x
+            param3[:, n1, n2] = lsq3.x
+    
+        idx2 = np.argmin(cost_remain) 
+        cost.append(cost1+cost_remain[idx2])
+        
+    idx1 = np.argmin(cost)
+    return idx1, idx2, cost[idx1], param1[:, idx1], param2[:, idx1, idx2], param3[:, idx1, idx2]
+            '''
